10804ad01f17d2f23e18f859a2d3a164a9ab989c
Johan Restrepo <62318029+JohanRestrepo19@users.noreply.github.com>
2024-04-09 18:13:33 -0500

    feat(db): add seeder file.

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -7,3 +7,32 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+FEATURES_AMOUNT = 4
+COMMETS_AMOUNT = 10
+
+FEATURES_AMOUNT.times.each do
+  Feature.create!(
+    {
+      external_id: Faker::Alphanumeric.unique.alphanumeric(number: 10, min_alpha: 2, min_numeric: 3),
+      magnitude: Faker::Number.between(from: -1.0, to: 10.0),
+      time: Faker::Number.number(digits: 13).to_s,
+      tsunami: Faker::Boolean.boolean,
+      mag_type: %w[md ml ms mw me mi mb mlg].sample,
+      title: Faker::Lorem.sentence,
+      place: Faker::Address.street_address,
+      longitude: Faker::Address.longitude,
+      latitude: Faker::Address.latitude,
+      external_url: Faker::Internet.url
+    }
+  )
+end
+
+COMMETS_AMOUNT.times.each do
+  Comment.create!(
+    {
+      body: Faker::Lorem.paragraph,
+      feature_id: rand(FEATURES_AMOUNT) + 1
+    }
+  )
+end
