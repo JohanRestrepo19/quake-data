d5df241ff70ef58dc58c1327102907fc7d839dd7
Johan Restrepo <62318029+JohanRestrepo19@users.noreply.github.com>
2024-04-09 22:59:47 -0500

    feat(models): add model validation rules.

--- backend/app/models/feature.rb
+++ backend/app/models/feature.rb
@@ -1,2 +1,8 @@
+# Class Model
 class Feature < ApplicationRecord
+  validates :title, :place, :mag_type, :latitude, :longitude, :external_url, presence: true
+
+  validates :magnitude, comparison: { greater_than_or_equal_to: -1.0, less_than_or_equal_to: 10.0 }
+  validates :latitude,  comparison: { greater_than_or_equal_to: -90.0, less_than_or_equal_to: 90.0 }
+  validates :longitude, comparison: { greater_than_or_equal_to: -180.0, less_than_or_equal_to: 180.0 }
 end
--- backend/db/migrate/20240408230935_create_comments.rb
+++ backend/db/migrate/20240408230935_create_comments.rb
@@ -4,7 +4,6 @@ class CreateComments < ActiveRecord::Migration[7.1]
       t.text :body
 
       t.timestamps
-
     end
 
     add_reference :comments, :feature, foreign_key: true
