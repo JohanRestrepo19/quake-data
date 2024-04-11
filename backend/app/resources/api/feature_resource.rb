69d0c998e4719eb3dd1b7e380c7bd09f84b1d1ad
Johan Restrepo <62318029+JohanRestrepo19@users.noreply.github.com>
2024-04-11 12:12:11 -0500

    fix(resources): modify feature resource.
    add coordinates attribute and remove relationships.

--- backend/app/resources/api/feature_resource.rb
+++ backend/app/resources/api/feature_resource.rb
@@ -2,11 +2,10 @@ module Api
   # Feature Resource
   class FeatureResource < JSONAPI::Resource
     immutable
-    has_many :comments
 
     attributes :external_id, :magnitude, :place, :time, :tsunami, :mag_type, :title
+    attribute :coordinates
 
-    # TODO: Check if I can filter by more than one value of filter filed.
     filter :mag_type
 
     exclude_links %i[self related]
